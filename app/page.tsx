b8c238ac664d4c013432188c149b6f0d66310e93
Armando Terrazas Gómez <A01640924@tec.mx>
2023-06-28 12:05:33 -0600

    restored fetches

--- app/page.tsx
+++ app/page.tsx
@@ -29,46 +29,11 @@ export default async function Home() {
   ];
   const randomIndex = Math.floor(Math.random() * greetingMessages.length);
 
-  // const skillsData = getSkills();
-  // const projectsData = getProjects("projects");
-  // const socialServiceData = getProjects("social-service");
-  // const awardsData = getProjects("awards");
-  // const [skills, projects, socialService, awards] = await Promise.all([skillsData, projectsData, socialServiceData, awardsData]);
-  const skills: SkillCardProps[] = [
-    {
-      title: "aaaa",
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original-wordmark.svg"
-    },
-  ];
-  const projects = [
-    {
-      name: "aaaa",
-      description: "aaaa",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/portfolio-f6166.appspot.com/o/projects%2Facademic.jpeg?alt=media&token=b385a5b4-f71f-49b6-9612-8eb4029f54fb",
-      url: "https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes",
-      dates: { start: "June 2023", end: "undefined" },
-    },
-  ];
-  const socialService = [
-    {
-      name: "Servicio",
-      description: "servicio",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/portfolio-f6166.appspot.com/o/social-service%2Fcentinela.jpeg?alt=media&token=2714450c-7e7e-4b73-9ecc-32b386f1d99f",
-      url: "https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes",
-      dates: { start: "June 2023", end: "June 2023" },
-    },
-  ];
-  const awards = [
-    {
-      name: 'Exilio',
-      description: 'aaaaaa',
-      image: 
-        'https://firebasestorage.googleapis.com/v0/b/portfolio-f6166.appspot.com/o/awards%2Fexiliogame.png?alt=media&token=4b81289a-f0fd-4652-87e4-422b93379176',
-      url: '',
-      dates: { start: 'June 2023', end: 'undefined' }
-    }]
+  const skillsData = getSkills();
+  const projectsData = getProjects("projects");
+  const socialServiceData = getProjects("social-service");
+  const awardsData = getProjects("awards");
+  const [skills, projects, socialService, awards] = await Promise.all([skillsData, projectsData, socialServiceData, awardsData]);
   return (
     <>
       <Hero greetingMessage={greetingMessages[randomIndex]} />
