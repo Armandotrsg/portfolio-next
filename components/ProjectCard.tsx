d50969c08eb716c7260cebd4799e5a5c2e38a037
Armando Terrazas Gómez <A01640924@tec.mx>
2023-06-28 19:14:10 -0600

    Fixed issue where the date was duplicated

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,6 +4,15 @@ import { ProjectProps } from "@/app/api/projects/[projects]/route";
 import Image from "next/image";
 
 export const ProjectCard = ({ name, description, image, url, dates }: ProjectProps) => {
+    const handleDate = (start: string, end?: string): string => {
+        if (end === "undefined") {
+            return `${start} - Present`;
+        } else if (start === end) {
+            return `${start}`;
+        } else {
+            return `${start} - ${end}`;
+        }
+    }
     if (url) {
         return (
             <Col sm={6} md={4}>
@@ -12,7 +21,7 @@ export const ProjectCard = ({ name, description, image, url, dates }: ProjectPro
                     <a href={url} target="_blank" rel="noreferrer">
                         <div className="proj-txtx">
                             <h4>{name}</h4>
-                            <span>{description}<br />{`${dates.start} - ${dates.end === "undefined" ? "Present" : dates.end}`}</span>
+                            <span>{description}<br />{handleDate(dates.start, dates.end)}</span>
                         </div>
                     </a>
                 </div>
@@ -25,7 +34,7 @@ export const ProjectCard = ({ name, description, image, url, dates }: ProjectPro
                     <Image src={image} alt={name} className="projectCard-Img" width={519} height={380} />
                     <div className="proj-txtx">
                         <h4>{name}</h4>
-                        <span>{description}<br />{`${dates.start} - ${dates.end === "undefined" ? "Present" : dates.end}`}</span>
+                        <span>{description}<br />{handleDate(dates.start, dates.end)}</span>
                     </div>
                 </div>
             </Col>
