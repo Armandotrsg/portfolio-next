4e9b1b8c895e03d1b8ec24dc073c6ca1d2f1e722
Armando Terrazas Gómez <A01640924@tec.mx>
2023-06-28 18:47:23 -0600

    Fixed issue where Arduino image wan't showing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     dangerouslyAllowSVG: true,
-    domains: ["firebasestorage.googleapis.com", "raw.githubusercontent.com"],
+    domains: ["firebasestorage.googleapis.com", "raw.githubusercontent.com", "cdn.worldvectorlogo.com"],
   },
 };
 
